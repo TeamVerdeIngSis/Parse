9ad3929b220732a67685ca867343c2ff7b5a2743
Federico <federicojkraft@gmail.com>
2024-09-19 17:27:10 -0300

    first commit

---
+++ src/test/kotlin/com/github/teamverdeingsis/parse/ParseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.teamverdeingsis.parse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
