54546926cdd0c0859f8773e75fe4bea572e958f7
Your GitHub Username <your.email@example.com>
2024-10-18 12:54:55 -0700

    changes on the docker-compose7

--- src/main/kotlin/com/github/teamverdeingsis/parse/ParseApplication.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/ParseApplication.kt
@@ -3,7 +3,7 @@ package com.github.teamverdeingsis.parse
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class])
+@SpringBootApplication
 class ParseApplication
 
 fun main(args: Array<String>) {
