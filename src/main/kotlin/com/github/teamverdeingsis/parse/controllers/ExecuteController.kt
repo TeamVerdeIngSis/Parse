0f5578580376f732d7c25ecc04536b437b4bc656
Your GitHub Username <your.email@example.com>
2024-11-17 13:45:38 -0800

    hey

--- src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
@@ -2,6 +2,7 @@ package com.github.teamverdeingsis.parse.controllers
 
 import com.github.teamverdeingsis.parse.dtos.ExecuteDTO
 import com.github.teamverdeingsis.parse.services.ExecuteService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,6 +22,12 @@ class ExecuteController(private val service: ExecuteService) {
         println("Executing code: $code")
         return service.executeSnippet(code, version)
     }
+
+        @GetMapping("/hey")
+        fun hey(): String {
+            println("AAAA")
+            return "Hey!"
+        }
 
     @PostMapping("/execute2")
     fun execute(
