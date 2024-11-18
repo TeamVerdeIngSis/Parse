59af2765c292b477700a162eff86678346f97c2e
Your GitHub Username <your.email@example.com>
2024-11-18 14:47:03 -0800

    pusheo para publicar

--- src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
@@ -2,6 +2,7 @@ package com.github.teamverdeingsis.parse.controllers
 
 import com.github.teamverdeingsis.parse.dtos.ExecuteDTO
 import com.github.teamverdeingsis.parse.services.ExecuteService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -22,10 +23,12 @@ class ExecuteController(private val service: ExecuteService) {
         println("Executing code: $code")
         return service.executeSnippet(code, version)
     }
+    private val logger = LoggerFactory.getLogger(ExecuteController::class.java)
 
     @GetMapping("/hola")
     fun hey(): String {
-        println("AAAA")
+        println("Jijo")
+        logger.info("Executing /hola endpoint")
         return service.hola()
     }
 
