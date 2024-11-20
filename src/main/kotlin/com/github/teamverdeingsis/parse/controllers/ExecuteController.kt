c26f60cf4f2f358892ad68173b24a9be70c985bb
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-11-20 19:48:58 -0300

    agrego tests

--- src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/controllers/ExecuteController.kt
@@ -1,6 +1,7 @@
 package com.github.teamverdeingsis.parse.controllers
 
 import com.github.teamverdeingsis.parse.dtos.ExecuteDTO
+import com.github.teamverdeingsis.parse.dtos.TestDto
 import com.github.teamverdeingsis.parse.services.ExecuteService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
@@ -21,6 +22,16 @@ class ExecuteController(private val service: ExecuteService) {
         val version = request.version
         println("Executing code: $code")
         return service.executeSnippet(code, version)
+    }
+
+    @PostMapping("/test")
+    fun test(@RequestBody testDto: TestDto): List<String> {
+        val version = testDto.version
+        val snippetId = testDto.snippetId
+        val inputs = testDto.inputs
+        val outputs = testDto.outputs
+
+        return service.test(version, snippetId, inputs, outputs)
     }
     private val logger = LoggerFactory.getLogger(ExecuteController::class.java)
 
--- src/main/kotlin/com/github/teamverdeingsis/parse/services/ExecuteService.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/services/ExecuteService.kt
@@ -18,6 +18,7 @@ class ExecuteService {
     fun hola(): String {
         return "Hola"
     }
+
     fun executeSnippet(code: String, version: String): String {
         val codeToInputStream: InputStream = code.byteInputStream()
         val reader = Reader(codeToInputStream)
@@ -34,5 +35,46 @@ class ExecuteService {
 
         val result = interpreter.interpret()
         return result.toString()
+    }
+
+        fun test(version: String, snippetId: String, inputs: List<String>, outputs: List<String>): List<String> {
+        val codeToInputStream: InputStream = snippetId.byteInputStream()
+        val reader = Reader(codeToInputStream)
+        val lexer = when (version) {
+            "1.1" -> LexerFactory().createLexer1_1(reader)
+            else -> LexerFactory().createLexer1_0(reader)
+        }
+        val parser = when (version) {
+            "1.1" -> ParserFactory().createParser1_1(lexer)
+            else -> ParserFactory().createParser1_0(lexer)
+        }
+        val printEmitter = PrintEmitter()
+        val interpreter = Interpreter(parser, ConsoleInputProvider(), printEmitter, ErrorCollector())
+
+        val result = interpreter.interpret()
+        return compareResults(listOf(result.toString()), outputs)
+    }
+
+    fun compareResults(results: List<String>, outputs: List<String>): List<String> {
+        val finalResults = mutableListOf<String>()
+
+        outputs.zip(results) { expected, actual ->
+            if (expected != actual) {
+                finalResults.add("Expected '$expected' but got '$actual'")
+            }
+        }
+
+        if (outputs.size < results.size) {
+            for (i in outputs.size until results.size) {
+                finalResults.add("Unexpected extra output: ${results[i]}")
+            }
+        }
+
+        else if (results.size < outputs.size) {
+            for (i in results.size until outputs.size) {
+                finalResults.add("Missing expected output: ${outputs[i]}")
+            }
+        }
+        return finalResults
     }
 }
