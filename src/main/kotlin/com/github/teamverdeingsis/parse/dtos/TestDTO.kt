8161c2e814e7fc3847f0ce7f95478a106255f920
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-11-22 16:36:24 -0300

    aksnkjansdas

--- src/main/kotlin/com/github/teamverdeingsis/parse/dtos/TestDTO.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/dtos/TestDTO.kt
@@ -2,7 +2,7 @@ package com.github.teamverdeingsis.parse.dtos
 
 data class TestDto(
     val version: String,
-    val snippetId: Long,
+    val snippetId: String,
     val inputs: List<String>,
     val outputs: List<String>
 )
