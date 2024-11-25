bc84876c0cdf85abda123b7e397e40dcec62e530
Your GitHub Username <your.email@example.com>
2024-11-25 12:25:17 -0800

    redis terminado

--- src/main/kotlin/com/github/teamverdeingsis/parse/config/RedisConfig.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/config/RedisConfig.kt
@@ -1,32 +1,19 @@
 package com.github.teamverdeingsis.parse.config
 
-import com.github.teamverdeingsis.parse.entity.SnippetMessage
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
-import org.springframework.data.redis.core.ReactiveRedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.RedisSerializer
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
 
 @Configuration
-class RedisConfig {
+class ConnectionFactory(@Value("\${spring.data.redis.host}") private val hostName: String, @Value("\${spring.data.redis.port}") private val port: Int) {
 
     @Bean
-    fun reactiveRedisTemplate(connectionFactory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, SnippetMessage> {
-        // Serializadores
-        val keySerializer: RedisSerializer<String> = StringRedisSerializer()
-        val valueSerializer: RedisSerializer<SnippetMessage> = GenericJackson2JsonRedisSerializer() as RedisSerializer<SnippetMessage>
-
-        // Crear contexto de serialización con pares correctos
-        val serializationContext = RedisSerializationContext
-            .newSerializationContext<String, SnippetMessage>(keySerializer)
-            .key(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer)) // Clave
-            .value(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer)) // Valor
-            .build()
-
-        // Crear ReactiveRedisTemplate
-        return ReactiveRedisTemplate(connectionFactory, serializationContext)
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(
+            RedisStandaloneConfiguration(hostName, port)
+        )
     }
 }
--- src/main/kotlin/com/github/teamverdeingsis/parse/entity/SnippetMessage.kt
+++ src/main/kotlin/com/github/teamverdeingsis/parse/entity/SnippetMessage.kt
@@ -1,8 +1,10 @@
 package com.github.teamverdeingsis.parse.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class SnippetMessage(
+    val snippetId: String,
     val userId: String,
-    val snippetId: String
 )
 
 data class Rule(
@@ -10,4 +12,39 @@ data class Rule(
     val name: String,
     val isActive: Boolean,
     val value: String? = null
+)
+
+
+enum class Conformance {
+    PENDING,
+    COMPLIANT,
+    NOT_COMPLIANT
+}
+
+data class UpdateConformanceRequest(
+    val snippetId: String,
+    val conformance: Conformance
+)
+
+
+data class LintingConfig(
+    @JsonProperty("identifier_format")
+    var identifierFormat: String = "none", // Default to none
+    @JsonProperty("mandatory-variable-or-literal-in-println")
+    var mandatoryVariableOrLiteral: String = "none",
+    @JsonProperty("read-input-with-simple-argument")
+    var readInputWithSimpleArgument: String = "none",
+)
+
+data class FormattingConfig(
+    @JsonProperty("space-before-colon")
+    var spaceBeforeColon: Boolean = false,
+    @JsonProperty("space-after-colon")
+    var spaceAfterColon: Boolean = false,
+    @JsonProperty("space-around-equals")
+    var spaceAroundEquals: Boolean = false,
+    @JsonProperty("newline-before-println")
+    var newlineBeforePrintln: Int = 0,
+    @JsonProperty("indentation")
+    var indentation: Int = 4,
 )
